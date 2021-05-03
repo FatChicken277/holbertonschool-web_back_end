500936a36972bcd0217538edd6b096a9dcde7657
FatChicken277 <aramirez@truora.com>
2021-05-02 21:09:12 -0500

    add 3-lru_cache.py

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""3-lru_cache.py module"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """This class represent a caching system"""
+
+    def __init__(self):
+        """Init"""
+
+        super().__init__()
+        self.least = []
+
+    def put(self, key, item):
+        """Assign to the dictionary"""
+
+        if key and item:
+            if key in self.cache_data:
+                self.least.remove(key)
+
+            self.cache_data[key] = item
+            self.least.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            least = self.least.pop(0)
+            del self.cache_data[least]
+            print("DISCARD:", least)
+
+    def get(self, key):
+        """Return the value linked to the key"""
+
+        return self.cache_data.get(key)
