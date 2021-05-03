7728287b2545cb291d7efc3c937b9260f3465321
FatChicken277 <aramirez@truora.com>
2021-05-02 22:56:41 -0500

    add counter

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,20 +3,36 @@
 
 import redis
 import uuid
+from functools import wraps
 from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """Returns a Callable"""
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def counter(self, *args, **kwds):
+        """counter wrapped function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+
+    return counter
 
 
 class Cache():
     """Cache class"""
 
     def __init__(self) -> None:
-        """class constructor"""
+        """Class constructor"""
 
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """generate a random key and store in redis"""
+        """Generate a random key and store in redis"""
 
         key = str(uuid.uuid4())
         self._redis.mset({key: data})
@@ -25,18 +41,18 @@ class Cache():
 
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[
             str, bytes, int, float]:
-        """method that take a key string and an optional Callable"""
+        """Method that take a key string and an optional Callable"""
 
         if fn:
             return fn(self._redis.get(key))
         return self._redis.get(key)
 
     def get_str(self, data: str) -> str:
-        """return a string correct conversion"""
+        """Return a string correct conversion"""
 
         return self._redis.get(data).decode('utf-8')
 
     def get_int(self, data: str) -> int:
-        """return a int correct conversion"""
+        """Return a int correct conversion"""
 
         return int(self._redis.get(data))
