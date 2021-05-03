b394775c0a521bfd775ae3e2064f36ed2e39bcea
FatChicken277 <aramirez@truora.com>
2021-05-02 22:30:44 -0500

    fix typing

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Any
+from typing import Union
 
 
 class Cache():
@@ -15,7 +15,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a random key and store in redis"""
 
         key = str(uuid.uuid4())
