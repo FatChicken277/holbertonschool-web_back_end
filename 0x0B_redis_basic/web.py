c39885ea645f0dbf1912ae92fc4ba33fda03c6d6
FatChicken277 <aramirez@truora.com>
2021-05-02 23:37:41 -0500

    add web.py

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""web module"""
+
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+
+def access_counter(method: Callable) -> Callable:
+    """access_counter"""
+
+    @wraps(method)
+    def count_and_expirate(*args, **kwds):
+        """count_and_expirate wrapped function"""
+        r = redis.Redis()
+        url = r.get(args[0])
+
+        if not url:
+            url = method(*args, **kwds)
+            r.setex(args[0], 10, url)
+
+        r.incr('count:' + url)
+        return url
+
+    return count_and_expirate
+
+
+@access_counter
+def get_page(url: str) -> str:
+    """Obtain HTML content of a URL and returns it"""
+    return requests.get(url).text
