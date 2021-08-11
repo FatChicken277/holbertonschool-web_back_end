a34f9ed4582e29253780d086fa844863a6155865
FatChicken277 <aramirez@truora.com>
2021-08-11 16:50:25 -0500

    fix multiplication

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -6,4 +6,4 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index"""
-    return ((page - 1 * page_size), page * page_size)
+    return (((page - 1) * page_size), page * page_size)
