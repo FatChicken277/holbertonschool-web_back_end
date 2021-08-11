474f12c40151750597d6c7f88d318e0d5eafe9ec
FatChicken277 <aramirez@truora.com>
2021-08-11 16:47:28 -0500

    Add 0-simple_helper_function task

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+0-simple_helper_function.py - This module contains index_range method.
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of size two containing a start index and an end index"""
+    return ((page - 1 * page_size), page * page_size)
