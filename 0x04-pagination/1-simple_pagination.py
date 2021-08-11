3cab748a03e2cd11b568b7c6668715d5688dc95e
FatChicken277 <aramirez@truora.com>
2021-08-11 17:13:46 -0500

    add index_range method

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -8,6 +8,11 @@ import math
 from typing import List
 
 
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of size two containing a start index and an end index."""
+    return (((page - 1) * page_size), page * page_size)
+
+
 class Server:
     """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
@@ -16,7 +21,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """Cached dataset.
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -27,7 +32,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return elements using pagination.
+        """
         assert(type(page) is int and page > 0)
         assert(type(page_size) is int and page_size > 0)
 
-        return self.dataset()[(page - 1) * page_size:page * page_size]
+        s, e = index_range(page, page_size)
+
+        return self.dataset()[s:e]
