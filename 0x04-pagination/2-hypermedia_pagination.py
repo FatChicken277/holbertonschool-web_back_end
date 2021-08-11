d37bf58fc0625bf70a3655ac734a58cdbf2f1092
FatChicken277 <aramirez@truora.com>
2021-08-11 17:33:14 -0500

    Add 2-hypermedia_pagination task

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" 1-simple_pagination.py
-This module implements get_page method.
+""" 2-hypermedia_pagination.py
+This module implements get_hyper method.
 """
 
 import csv
@@ -40,3 +40,17 @@ class Server:
         s, e = index_range(page, page_size)
 
         return self.dataset()[s:e]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Returns a dictionary containing pagination info.
+        """
+        data = self.get_page(page, page_size)
+        total_pages = int(len(self.dataset()) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages,
+        }
