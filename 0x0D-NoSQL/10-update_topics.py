f7a9bfd25dfac0922432028d639db4b6771c2654
FatChicken277 <aramirez@truora.com>
2021-09-07 11:27:32 -0500

    add 10-update_topics.py

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a method use to implement mongodb"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name"""
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
