0ec28e91a7ad1d690be2424d9e8ec8a4b0203131
FatChicken277 <aramirez@truora.com>
2021-09-14 18:47:13 -0500

    use allSettled instead all

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,9 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      console.log(values);
-    })
-    .catch((err) => console.log(err));
+  const userResponse = signUpUser(firstName, lastName);
+  const photoResponse = uploadPhoto(fileName);
+
+  return Promise.allSettled([userResponse, photoResponse]).then((Values) =>
+    console.log(Values)
+  );
 }
