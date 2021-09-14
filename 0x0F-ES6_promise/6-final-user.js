60fccc55297468c81891cb125602d84e28237b6c
FatChicken277 <aramirez@truora.com>
2021-09-14 18:11:51 -0500

    test

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,6 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => console.log(values))
+    .then((values) => {
+      console.log(values);
+    })
     .catch((err) => console.log(err));
 }
