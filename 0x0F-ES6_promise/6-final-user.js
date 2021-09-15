a13505bc524fff28f34857853322d29a46ec0a53
FatChicken277 <aramirez@truora.com>
2021-09-14 19:10:15 -0500

    fix return value

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -5,7 +5,20 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userResponse = signUpUser(firstName, lastName);
   const photoResponse = uploadPhoto(fileName);
 
-  return Promise.allSettled([userResponse, photoResponse]).then(
-    (Values) => Values
-  );
+  return Promise.all([userResponse, photoResponse].map((p) => p.catch((e) => e))).then((values) => {
+    const responses = [];
+
+    for (let value of values) {
+      let status = 'fulfilled';
+
+      if (value instanceof Error) {
+        status = 'rejected';
+        value = value.toString();
+      }
+
+      responses.push({ status, value });
+    }
+
+    return responses;
+  });
 }
