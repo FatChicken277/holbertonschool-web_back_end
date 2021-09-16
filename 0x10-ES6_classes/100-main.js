36605af049ea8f8ca4b6ddab1c10c08011dff0d5
FatChicken277 <aramirez@truora.com>
2021-09-16 12:57:08 -0500

    fix 10-car

--- 0x10-ES6_classes/100-main.js
+++ 0x10-ES6_classes/100-main.js
@@ -1,10 +1,7 @@
 import EVCar from './100-evcar';
-import Car from './10-car';
 
 const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
 console.log(ec1);
-console.log(ec1 instanceof Car);
 
 const ec2 = ec1.cloneCar();
 console.log(ec2);
-console.log(ec2 instanceof Car);
