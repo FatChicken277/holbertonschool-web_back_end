6993f4883212ff554a7982fb7dddaca1aed8f288
FatChicken277 <aramirez@truora.com>
2021-09-29 16:38:07 -0500

    testing

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,29 @@
 const assert = require('assert');
-
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('round a and b and return the sum of it', () => {
+  it('should return the sum of 2 integers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(10, 3), 13);
+  });
+
+  it('should return the sum of 2 floats', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
-    assert.strictEqual(calculateNumber(0.4, 0.5), 1);
+  });
+
+  it('should return the sum of 2 negative integers', () => {
+    assert.strictEqual(calculateNumber(-1, 3), 2);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+  });
+
+  it('should return the sum of 2 negative floats', () => {
+    assert.strictEqual(calculateNumber(-1.7, 3), 1);
+    assert.strictEqual(calculateNumber(-1.2, -3.5), -4);
+  });
+
+  it('should return the sum of 2 strings', () => {
+    assert.strictEqual(calculateNumber('-1.7', '3'), 1);
   });
 });
