2d09837300da001a875f893288e24ae7e6be7a22
FatChicken277 <aramirez@truora.com>
2021-09-29 15:36:15 -0500

    use arrow functions

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('should return the sum of 2 integers', function () {
+describe('calculateNumber', () => {
+  it('should return the sum of 2 integers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(10, 3), 13);
   });
 
-  it('should return the sum of 2 floats', function () {
+  it('should return the sum of 2 floats', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
-  it('should return the sum of 2 negative integers', function () {
+  it('should return the sum of 2 negative integers', () => {
     assert.strictEqual(calculateNumber(-1, 3), 2);
     assert.strictEqual(calculateNumber(-1, -3), -4);
   });
 
-  it('should return the sum of 2 negative floats', function () {
+  it('should return the sum of 2 negative floats', () => {
     assert.strictEqual(calculateNumber(-1.7, 3), 1);
     assert.strictEqual(calculateNumber(-1.2, -3.5), -4);
   });
