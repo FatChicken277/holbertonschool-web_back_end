04d5bc95bae97c953b1b72d8b67359da0ab3fbef
FatChicken277 <aramirez@truora.com>
2021-09-29 16:45:19 -0500

    add more tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -5,9 +5,10 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('should return the sum of 2 numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(10, 3), 13);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(10, 3), 13);
     assert.strictEqual(calculateNumber(-1, 3), 2);
     assert.strictEqual(calculateNumber(-1, -3), -4);
     assert.strictEqual(calculateNumber(-1.7, 3), 1);
