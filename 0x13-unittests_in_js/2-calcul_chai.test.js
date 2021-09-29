739349692431d2a834a6f8c9b52fa6145d66446f
FatChicken277 <aramirez@truora.com>
2021-09-29 17:57:32 -0500

    use correct file

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul');
 
 describe('calculateNumber', function () {
   describe('SUM', function () {
