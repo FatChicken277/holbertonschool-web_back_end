e640148b3cf58c0649b5d0968ea7e7d296ab1b8d
FatChicken277 <aramirez@truora.com>
2021-09-29 15:53:50 -0500

    remove last change

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,25 +1,25 @@
-import { strictEqual } from 'assert';
-import calculateNumber from './0-calcul';
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('should return the sum of 2 integers', function () {
-    strictEqual(calculateNumber(1, 3), 4);
-    strictEqual(calculateNumber(10, 3), 13);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(10, 3), 13);
   });
 
   it('should return the sum of 2 floats', function () {
-    strictEqual(calculateNumber(1, 3.7), 5);
-    strictEqual(calculateNumber(1.2, 3.7), 5);
-    strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
   it('should return the sum of 2 negative integers', function () {
-    strictEqual(calculateNumber(-1, 3), 2);
-    strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1, 3), 2);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
   });
 
   it('should return the sum of 2 negative floats', function () {
-    strictEqual(calculateNumber(-1.7, 3), 1);
-    strictEqual(calculateNumber(-1.2, -3.5), -4);
+    assert.strictEqual(calculateNumber(-1.7, 3), 1);
+    assert.strictEqual(calculateNumber(-1.2, -3.5), -4);
   });
 });
