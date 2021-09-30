b77802e89a2951e6b96a6e0520915bf611a38fcb
FatChicken277 <aramirez@truora.com>
2021-09-29 19:44:32 -0500

    fix tets case

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,15 +1,15 @@
-const { spy } = require('sinon');
 const { expect } = require('chai');
+const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function () {
-  const calculateNumberSpy = spy(Utils, 'calculateNumber');
+  const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
 
   it('should send payment request', function () {
     sendPaymentRequestToApi(100, 20);
 
-    expect(calculateNumberSpy.calledOnce);
+    expect(calculateNumberSpy.calledOnce).to.equal(true);
     expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
 
     calculateNumberSpy.restore();
