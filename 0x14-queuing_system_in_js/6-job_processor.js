4d5ee5b0a157e3e04288ec211e36460f69620fbb
FatChicken277 <aramirez@truora.com>
2021-10-05 18:36:05 -0500

    add 6-job_processor

--- 0x14-queuing_system_in_js/6-job_processor.js
+++ 0x14-queuing_system_in_js/6-job_processor.js
@@ -3,6 +3,7 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 function sendNotification(phoneNumber, message) {
+  // eslint-disable-next-line no-console
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
