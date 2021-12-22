892ce042551d20cdde27142eccbe2b78f91cbf1d
FatChicken277 <aramirez@truora.com>
2021-12-21 23:23:19 -0500

    fix import

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module contains a basic flask app"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
