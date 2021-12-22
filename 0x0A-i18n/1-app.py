f263ca85451c6ff891da70a9e78457ab0beca34c
FatChicken277 <aramirez@truora.com>
2021-12-21 22:25:23 -0500

    add 1-app.py

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""This module contains a basic flask app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object('1-app.Config')
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """index route"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    # python -m api.v1.app
+    app.run(host='0.0.0.0', port=5000)
